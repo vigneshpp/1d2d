75ba59783b2f22b537dcf30c49e6ed02f3ee38ef
vigneshpp <vigneshpp@users.noreply.github.com>
2021-01-09 22:28:09 +1300

    hostname port update

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 //Load HTTP module
 const http = require("http");
-// const hostname = '127.0.0.1';
-// const port = 8080;
 const bwipjs = require('bwip-js');
+//const hostname = '127.0.0.1';
+const port = 8080;
 
-//Create HTTP server and listen on port 3000 for requests
-
-http.createServer(function(req, res) {
+const server = http.createServer(function(req, res) {
   // If the url does not begin /?bcid= then 404.  Otherwise, we end up
   // returning 400 on requests like favicon.ico.
+
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url.indexOf('/?bcid=') != 0) {
       res.writeHead(404, { 'Content-Type':'text/plain' });
       res.end('BWIPJS: Unknown request format.', 'utf8');
@@ -16,10 +18,17 @@ http.createServer(function(req, res) {
       bwipjs.request(req, res); // Executes asynchronously
   }
 
-}).listen(3030);
+});
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at PORT:${port}/`);
+});
+
+// console.log(`Server now running!`);
+
+// //listen for request on port 3000, and as a callback function have the port listened on logged
+// server.listen (port, hostname, () => {
+// console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
