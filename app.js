924abc6ddbb56f558ce39e08001012c8523c1119
vigneshpp <vigneshpp@users.noreply.github.com>
2021-01-16 22:05:51 +1300

    host server changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Load HTTP module
 const http = require("http");
 const bwipjs = require('bwip-js');
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3200;
 
 const server = http.createServer(function(req, res) {
@@ -19,7 +19,10 @@ const server = http.createServer(function(req, res) {
 
 });
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
+//listen for request on port 3200, and as a callback function have the port listened on logged
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at Hostname:${hostname}/`);
+  console.log(`Server running at Port:${port}/`);
+  console.log(`Test link Barcode Code128: http://127.0.0.1:${port}/?bcid=code128&includetext&textalign=center&scale=3&text=BWR-PTD-G46-STW-1-1.5-5.6`);
+  console.log(`Test link QRCode: http://127.0.0.1:${port}/?bcid=qrcode&includetext&textalign=center&scale=9&text=test123123testtesttesttest`);
 });
